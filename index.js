851e7d85fa7948494f6910fb6e6a9a9e90945b1d
Vincent <bongrand.v91@gmail.com>
2021-01-07 19:26:22 +0100

    first commit miniserver en Express

---
+++ index.js
@@ -0,0 +1,110 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+let mongoClient;
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended : false
+}));
+
+const urlDb = 'mongodb://localhost:27017';
+const nameDb = 'exercice16';
+const urlConnect = 8888;
+
+let connect;
+
+app.use('/src', express.static(__dirname + '/public/src'));
+app.use('/img', express.static(__dirname + '/public/images'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+/*
+app.post('/connect', (req, res) => {
+  const user = req.body;
+  console.log(req.body)
+  if(user.email === db.user[0].email &&
+    user.mdp === db.user[0].password){
+      res.render('profile', {username : db.user[0].username});
+    } else {
+      res.render('index');
+    }
+});
+*/
+app.post('/connect', (req, res, next) => {
+  const user = req.body;
+  let find = false;
+  let identity;
+  const collection = mongoClient.collection('userbanks');
+  collection.find().toArray((err,data) => {
+    data.forEach(object => {
+      if(object.email === user.email && object.mdp === user.mdp){
+        find = true;
+        connect = true;
+        identity = object;
+      }
+    });
+    if(find){
+      res.render('profile', {username : identity.username});
+    } else {
+      res.render('index');
+    }
+  });
+});
+
+app.post('/create', (req, res, next) => {
+  const user = req.body;
+  let find = false;
+  let identity;
+  const collection = mongoClient.collection('userbanks');
+  collection.find().toArray((err,data) => {
+    data.forEach(object => {
+      if(object.email === user.email || object.username === user.username){
+        find = true;
+      }
+    });
+    if(find){
+      res.render('newaccount');
+    } else {
+      user.id = data.length + 1;
+      collection.insertOne(user, (err, res) => {
+        if(err) throw err;
+        console.log('1 user inserted');
+      })
+      connect = true;
+      res.render('profile', {username : user.username});
+    }
+  });
+});
+
+app.get('/newaccount', (req, res) => {
+  res.render('newaccount');
+});
+
+app.get('/newpass', (req, res) => {
+  res.render('newpass');
+});
+
+app.post('/messpass', (req, res) => {
+  res.render('messpass');
+});
+
+app.post('/index', (req, res) => {
+  res.render('index');
+})
+
+MongoClient.connect(urlDb, function(err, client){
+  if(err){
+      return
+  }
+  mongoClient = client.db(nameDb);
+  app.listen(urlConnect, () => {
+    console.log('Serveur lancé sur le port 8888');
+  });
+});
